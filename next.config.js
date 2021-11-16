883283e5374d1d09bcce65884649431df56ab67e
theZiggurat <frappyd@gmail.com>
2021-11-16 00:54:44 -0500

    created wasm lib utilizing naga shader compiler

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@
 module.exports = {
   reactStrictMode: true,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    
+    config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm'
+    config.experiments.asyncWebAssembly = true 
+
     config.module.rules.push({
       test: /\.wgsl$/i,
       use: 'raw-loader',
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import React, { useEffect } from 'react'
-import '../styles/create.css'
-import '../styles/prism-custom.css'
-import 'react-tabs/style/react-tabs.css';
+import type { AppProps } from 'next/app'
+import dynamic from 'next/dynamic'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
 import GPU, { GPUInitResult } from '../src/gpu/gpu'
 import ShdrAlert from '../src/components/warning'
 
+import '../styles/globals.css'
+import '../styles/create.css'
+import '../styles/prism-custom.css'
+import 'react-tabs/style/react-tabs.css';
+
+import init, {test} from '../naga-compiler/pkg/naga_compiler'
+
+//const NagaShader = dynamic(() => import('../naga-compiler/pkg/naga_compiler'))
 
 const theme = extendTheme({
   
@@ -27,6 +32,17 @@ function MyApp({ Component, pageProps }: AppProps) {
   //   const initGPU = async () =>  setGPUInitResult(await GPU.init())
   //   initGPU()
   // }, [])
+
+  useEffect(() => {
+    init()
+      .then(() => {
+        let result = test()
+        if(result)
+          console.log("result good!", result)
+        else
+          console.log("no good")
+      })
+  }, [])
 
   return (
     <ChakraProvider theme={theme}>
